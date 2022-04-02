0ff42010761388eb2fe7cd180ca0e60631929c70
pablo <insomnionebulous@gmail.com>
2022-04-02 19:24:43 -0300

    avance template

--- src/templateConfig.js
+++ src/templateConfig.js
@@ -2,130 +2,55 @@ import presentationImg from "./assets/images/perfil.png"
 import cv from "./assets/curriculum.pdf"
 
 export const templateConfig = {
-	folders: [
-		{
-			name: "Sobre Mi",
-			files: [
-				{
-					icon: "PersonalCard",
-					fileName: "Presentacion",
-					screen: "Presentation",
-					screenConfig: {
-						firstLine: "Hola, soy Pablo Peña",
-						secondLine: "Desarrollador Front-End",
-						imageSrc: presentationImg,
-						cvSrc: cv,
-					},
-				},
-			],
-		},
-		{
-			name: "Proyectos",
-			files: [
-				{ icon: "GitHub", fileName: "Portafolio" },
-				{ icon: "GitHub", fileName: "Meli Clon" },
-			],
-		},
-		{
-			name: "Aportes",
-			files: [
-				{ icon: "GitFork", fileName: "Meli Clon" },
-				{ icon: "GitFork", fileName: "Meli Clon2" },
-				{ icon: "GitFork", fileName: "Meli Clon3" },
-			],
-		},
-		{
-			name: "Sobre Mi",
-			files: [
-				{
-					icon: "PersonalCard",
-					fileName: "Presentacion",
-					screen: "Presentation",
-					screenConfig: {
-						firstLine: "Hola, soy Pablo Peña",
-						secondLine: "Desarrollador Front-End",
-						imageSrc: presentationImg,
-						cvSrc: cv,
-					},
-				},
-			],
-		},
-		{
-			name: "Proyectos",
-			files: [
-				{ icon: "GitHub", fileName: "Portafolio" },
-				{ icon: "GitHub", fileName: "Meli Clon" },
-			],
-		},
-		{
-			name: "Aportes",
-			files: [
-				{ icon: "GitFork", fileName: "Meli Clon" },
-				{ icon: "GitFork", fileName: "Meli Clon2" },
-				{ icon: "GitFork", fileName: "Meli Clon3" },
-			],
-		},
-		{
-			name: "Sobre Mi",
-			files: [
-				{
-					icon: "PersonalCard",
-					fileName: "Presentacion",
-					screen: "Presentation",
-					screenConfig: {
-						firstLine: "Hola, soy Pablo Peña",
-						secondLine: "Desarrollador Front-End",
-						imageSrc: presentationImg,
-						cvSrc: cv,
-					},
-				},
-			],
-		},
-		{
-			name: "Proyectos",
-			files: [
-				{ icon: "GitHub", fileName: "Portafolio" },
-				{ icon: "GitHub", fileName: "Meli Clon" },
-			],
-		},
-		{
-			name: "Aportes",
-			files: [
-				{ icon: "GitFork", fileName: "Meli Clon" },
-				{ icon: "GitFork", fileName: "Meli Clon2" },
-				{ icon: "GitFork", fileName: "Meli Clon3" },
-			],
-		},
-		{
-			name: "Sobre Mi",
-			files: [
-				{
-					icon: "PersonalCard",
-					fileName: "Presentacion",
-					screen: "Presentation",
-					screenConfig: {
-						firstLine: "Hola, soy Pablo Peña",
-						secondLine: "Desarrollador Front-End",
-						imageSrc: presentationImg,
-						cvSrc: cv,
-					},
-				},
-			],
-		},
-		{
-			name: "Proyectos",
-			files: [
-				{ icon: "GitHub", fileName: "Portafolio" },
-				{ icon: "GitHub", fileName: "Meli Clon" },
-			],
-		},
-		{
-			name: "Aportes",
-			files: [
-				{ icon: "GitFork", fileName: "Meli Clon" },
-				{ icon: "GitFork", fileName: "Meli Clon2" },
-				{ icon: "GitFork", fileName: "Meli Clon3" },
-			],
-		},
-	],
+  socialMedia: [
+    {
+      icon: "linkedin",
+      url: "https://www.linkedin.com/in/pablo-pe%C3%B1a-15619420a/",
+    },
+    {
+      icon: "GitHub",
+      url: "https://github.com/sebas-pena",
+    },
+    {
+      icon: "ïnstagram",
+      url: "https://www.instagram.com/sebastian_.pena/",
+    },
+    {
+      icon: "Gmail",
+      url: "mailto://sebapena2002@gmail.com",
+    },
+  ],
+  folders: [
+    {
+      name: "Sobre Mi",
+      files: [
+        {
+          icon: "PersonalCard",
+          fileName: "Presentacion",
+          screen: "Presentation",
+          screenConfig: {
+            firstLine: "Hola, soy Pablo Peña",
+            secondLine: "Desarrollador Front-End",
+            imageSrc: presentationImg,
+            cvSrc: cv,
+          },
+        },
+      ],
+    },
+    {
+      name: "Proyectos",
+      files: [
+        { icon: "GitHub", fileName: "Portafolio" },
+        { icon: "GitHub", fileName: "Meli Clon" },
+      ],
+    },
+    {
+      name: "Aportes",
+      files: [
+        { icon: "GitFork", fileName: "Meli Clon" },
+        { icon: "GitFork", fileName: "Meli Clon2" },
+        { icon: "GitFork", fileName: "Meli Clon3" },
+      ],
+    },
+  ],
 }
