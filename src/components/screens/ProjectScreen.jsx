4c9537f5aa8481679bf175504dab295496d21af6
pablo <insomnionebulous@gmail.com>
2022-04-04 15:35:45 -0300

    avance pagina proyecto

--- src/components/screens/ProjectScreen.jsx
+++ src/components/screens/ProjectScreen.jsx
@@ -1,20 +1,52 @@
 import projectImg from "../../assets/images/meli-clon.png"
+import {
+  ExternalLinkIcon,
+  GitHubIcon,
+  ReactLogo,
+  SassLogo,
+} from "../../assets/svgs/icons"
 
 import "./projectScreen.css"
 
 export const ProjectScreen = () => {
-	return (
-		<div className="project-screen__ctn">
-			<h2>Project Name</h2>
-			<div
-				className="project-screen__images"
-				style={{ backgroundImage: `url(${projectImg})` }}
-			></div>
-			<div className="project-screen__links">
-				<a>Repositorio</a>
-				<a>Pagina</a>
-			</div>
-			<div className="project-screen__tech"></div>
-		</div>
-	)
+  return (
+    <div className="project-screen__ctn">
+      <h2>Clon de Mercado Libre</h2>
+      <div className="project-images__ctn">
+        <div className="project-screen__tech">
+          <div className="tech__icon react">
+            <ReactLogo size={38} color="#fff" />
+          </div>
+          <div className="tech__icon sass">
+            <SassLogo size={38} color="#fff" />
+          </div>
+        </div>
+
+        <div
+          className="project__image"
+          style={{ backgroundImage: `url(${projectImg})` }}
+        >
+          <div className="project-image__progress">En progreso</div>
+        </div>
+      </div>
+      <div className="project-screen__links">
+        <a
+          className="project__link-github"
+          target="_blank"
+          href="https://github.com/sebas-pena/meli-clone"
+        >
+          <GitHubIcon size={14} color="#333333" />
+          <span> Repositorio</span>
+        </a>
+        <a
+          className="project__link-page"
+          target="_blank"
+          href="https://meli-clon-pp.herokuapp.com/"
+        >
+          <ExternalLinkIcon size={14} color="#ffffff" />
+          <span> Página</span>
+        </a>
+      </div>
+    </div>
+  )
 }
