ce1996b0139d9f3fb658ac8db95d43362256a22b
Pablo Peña <insomnionebulous@gmail.com>
2022-04-05 01:20:07 -0300

    avance template project y reflow en presentation

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,54 +1,62 @@
-import { useState, useContext, useEffect } from "react"
+import { useState, useContext } from "react"
 import { FolderContext } from "../context/FolderContext"
 import {
-  ArrowIcon,
-  FolderIcon,
-  GitHubIcon,
-  GitForkIcon,
-  PersonalCardIcon,
+	ArrowIcon,
+	FolderIcon,
+	GitHubIcon,
+	GitForkIcon,
+	PersonalCardIcon,
 } from "../assets/svgs/icons"
+import { AppContext } from "../context/AppContext"
 
 export const Folder = ({ name, files, folderIndex }) => {
-  const handleOpenFolder = () => setOpenFolder(!openFolder)
-  const [openFolder, setOpenFolder] = useState(false)
-  const FoldersContext = useContext(FolderContext)
-  const Icons = {
-    PersonalCard: <PersonalCardIcon size={12} />,
-    GitHub: <GitHubIcon size={12} />,
-    GitFork: <GitForkIcon size={12} />,
-  }
+	const handleOpenFolder = () => setOpenFolder(!openFolder)
+	const [context, setContext] = useContext(AppContext)
+	const [openFolder, setOpenFolder] = useState(false)
+	const FoldersContext = useContext(FolderContext)
+	const Icons = {
+		PersonalCard: <PersonalCardIcon size={12} />,
+		GitHub: <GitHubIcon size={12} />,
+		GitFork: <GitForkIcon size={12} />,
+	}
 
-  const handleChangeFile = ({ currentTarget }) => {
-    FoldersContext.current.classList.toggle("active")
-    currentTarget.classList.toggle("active")
-    FoldersContext.current = currentTarget
-    console.log(currentTarget.getAttribute("folder"))
-    console.log(currentTarget.getAttribute("file"))
-  }
+	const handleChangeFile = ({ currentTarget }) => {
+		FoldersContext.current.classList.toggle("active")
+		currentTarget.classList.toggle("active")
+		FoldersContext.current = currentTarget
 
-  return (
-    <div className={"aside__section-folder " + (openFolder ? "open" : "")}>
-      <button
-        className={"aside__folder-header " + (openFolder ? "open" : "")}
-        onClick={handleOpenFolder}
-      >
-        <ArrowIcon size={10} />
-        <FolderIcon size={14} />
-        <h3>{name}</h3>
-      </button>
-      <div className="aside__folder-scope"></div>
-      {files.map(({ icon, fileName }, index) => (
-        <button
-          className="aside__folder-file"
-          key={fileName}
-          folder={folderIndex}
-          file={index}
-          onClick={handleChangeFile}
-        >
-          {Icons[icon]}
-          <p>{fileName}</p>
-        </button>
-      ))}
-    </div>
-  )
+		setContext({
+			...context,
+			display: [
+				currentTarget.getAttribute("folder"),
+				currentTarget.getAttribute("file"),
+			],
+		})
+	}
+
+	return (
+		<div className={"aside__section-folder " + (openFolder ? "open" : "")}>
+			<button
+				className={"aside__folder-header " + (openFolder ? "open" : "")}
+				onClick={handleOpenFolder}
+			>
+				<ArrowIcon size={10} />
+				<FolderIcon size={14} />
+				<h3>{name}</h3>
+			</button>
+			<div className="aside__folder-scope"></div>
+			{files.map(({ icon, fileName }, index) => (
+				<button
+					className="aside__folder-file"
+					key={fileName}
+					folder={folderIndex}
+					file={index}
+					onClick={handleChangeFile}
+				>
+					{Icons[icon]}
+					<p>{fileName}</p>
+				</button>
+			))}
+		</div>
+	)
 }
--- src/components/screens/ProjectScreen.jsx
+++ src/components/screens/ProjectScreen.jsx
@@ -1,47 +1,47 @@
-import projectImg from "../../assets/images/meli-clon.png"
 import {
-  ExternalLinkIcon,
-  GitHubIcon,
-  ReactLogo,
-  SassLogo,
+	ExternalLinkIcon,
+	GitHubIcon,
+	ReactLogo,
+	SassLogo,
 } from "../../assets/svgs/icons"
 
-export const ProjectScreen = () => {
-  return (
-    <div className="project-screen__ctn">
-      <h2>Clon de Mercado Libre</h2>
-      <div
-        className="project__image"
-        style={{ backgroundImage: `url(${projectImg})` }}
-      >
-        <div className="project-image__progress">En progreso</div>
-      </div>
-      <div className="project-screen__links">
-        <a
-          className="project__link-github"
-          target="_blank"
-          href="https://github.com/sebas-pena/meli-clone"
-        >
-          <GitHubIcon size={14} color="#333333" />
-          <span> Repositorio</span>
-        </a>
-        <a
-          className="project__link-page"
-          target="_blank"
-          href="https://meli-clon-pp.herokuapp.com/"
-        >
-          <ExternalLinkIcon size={14} color="#ffffff" />
-          <span> Página</span>
-        </a>
-      </div>
-      <div className="project-screen__tech">
-        <div className="tech__icon react">
-          <ReactLogo size={38} color="#fff" />
-        </div>
-        <div className="tech__icon sass">
-          <SassLogo size={38} color="#fff" />
-        </div>
-      </div>
-    </div>
-  )
+export const ProjectScreen = ({
+	title,
+	image,
+	icons,
+	repoLink,
+	deployLink,
+}) => {
+	const techIcons = {
+		react: <ReactLogo size={38} color="#fff" />,
+		sass: <SassLogo size={38} color="#fff" />,
+	}
+	return (
+		<div className="project-screen__ctn">
+			<h2>{title}</h2>
+			<div
+				className="project__image"
+				style={{ backgroundImage: `url(${image})` }}
+			>
+				<div className="project-image__progress">En progreso</div>
+			</div>
+			<div className="project-screen__links">
+				<a className="project__link-github" target="_blank" href={repoLink}>
+					<GitHubIcon size={14} color="#333333" />
+					<span> Repositorio</span>
+				</a>
+				<a className="project__link-page" target="_blank" href={deployLink}>
+					<ExternalLinkIcon size={14} color="#ffffff" />
+					<span> Página</span>
+				</a>
+			</div>
+			<div className="project-screen__tech">
+				{icons.map((tech, index) => (
+					<div key={tech + index} className={`tech__icon ${tech}`}>
+						{techIcons[tech]}
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
